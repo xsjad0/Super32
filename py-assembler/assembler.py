028af03490787f10ef54501b912fe6711f7da350
Marcel Freiberg <marcel.freiberg@outlook.com>
2019-11-28 09:04:49 +0100

    Adding a function to validate the parsed lines

--- py-assembler/assembler.py
+++ py-assembler/assembler.py
@@ -42,7 +42,7 @@ def read_json(path):
 def read_code(path):
     try:
         with Manager(path, "r") as file:
-            code = file.readlines()  # file.read().splitlines()
+            code = file.readlines()
             logger.info("code file loaded")
             return code
 
@@ -56,7 +56,6 @@ class Parser():
         pass
 
     def parse(self, code, commands, registers):
-
         bitcode = []
         for line in code:
             if line == '\n':
@@ -76,6 +75,14 @@ class Parser():
         joind = ''.join(bitcode)
         return ''.join(bitcode)
 
+    def __checkLine(self, line):
+        if len(line) != 32:
+            raise Exception('Parsing error')
+
+        for bit in line:
+            if bit not in [0, 1]:
+                raise Exception('Parsing error')
+
     def __parseArithmetic(self, line, arithmetic, registers):
         for cmd in arithmetic:
             line = line.replace(cmd, arithmetic[cmd])
@@ -87,6 +94,8 @@ class Parser():
                     line = line.replace((reg + ','), registers[reg])
             if ' ' in line:
                 line = line.replace(' ', '')
+
+        self.__checkLine(line)
 
         return [line]
 
@@ -101,6 +110,8 @@ class Parser():
                     line = line.replace((reg + ','), registers[reg])
             if ' ' in line:
                 line = line.replace(' ', '')
+
+        self.__checkLine(line)
 
         return [line]
 
@@ -117,6 +128,8 @@ class Parser():
 
             if ' ' in line:
                 line = line.replace(' ', '')
+
+        self.__checkLine(line)
 
         return [line]
 
